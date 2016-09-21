4a4cf0d658c2c3ed3796f11c84aaae1df822b42b
Nattawat Jaikaew <Kiragames@Kiragamess-MacBook-Air.local>
2016-09-22 00:53:08 +0700

    Prepare for Grunt-nw-builder

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    nwjs: {
+      options: {
+        version: '0.0.1',
+        buildDir: './build', // Where the build version of my NW.js app is saved
+        macIcns: './icon.icns', // Path to the Mac icon file
+        platforms: ['osx64'], // These are the platforms that we want to build
+        flavor: 'normal'
+      },
+      src: './src/**/*' // Your NW.js app
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-nw-builder');
+  grunt.registerTask('default', ['nwjs']);
+};
